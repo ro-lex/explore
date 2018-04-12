3064c674743b2648f71d02b577d95ff72d364ad1
Roshan Sam Alex <swift.rolex@gmail.com>
2018-04-11 21:06:05 -0500

    AWS S3 List all buckets

--- aws-s3/src/main/java/com/rolex/explore/awss3/AwsS3Application.java
+++ aws-s3/src/main/java/com/rolex/explore/awss3/AwsS3Application.java
@@ -2,11 +2,18 @@ package com.rolex.explore.awss3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.rolex.explore.awss3.service.AwsService;
 
 @SpringBootApplication
 public class AwsS3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AwsS3Application.class, args);
+		ApplicationContext appContext = SpringApplication.run(AwsS3Application.class, args);
+		System.out.println("Hello, how are you?");
+		AwsService awsService = appContext.getBean(AwsService.class);
+		awsService.getAllS3Buckets();
+		System.out.println("I am good.");
 	}
 }
