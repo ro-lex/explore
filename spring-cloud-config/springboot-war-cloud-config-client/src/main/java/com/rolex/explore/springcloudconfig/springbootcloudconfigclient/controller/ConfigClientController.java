8b6db1ee1f3101a100bdfb78e6af758b01834473
Roshan Sam Alex <swift.rolex@gmail.com>
2018-08-26 19:00:43 -0500

    Pointing to multiple property files

--- spring-cloud-config/springboot-war-cloud-config-client/src/main/java/com/rolex/explore/springcloudconfig/springbootcloudconfigclient/controller/ConfigClientController.java
+++ spring-cloud-config/springboot-war-cloud-config-client/src/main/java/com/rolex/explore/springcloudconfig/springbootcloudconfigclient/controller/ConfigClientController.java
@@ -1,7 +1,9 @@
 package com.rolex.explore.springcloudconfig.springbootcloudconfigclient.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +14,14 @@ public class ConfigClientController {
 	
 	@Value("${greeting-message}")
 	private String propertyValue;
+	
+	@Autowired
+	private Environment environment;
 
 	
 	@RequestMapping(path = "/configs/applications/one", method = RequestMethod.GET)
 	public String getApplicationConfig() {
-		return "Config as seen on client deployable on Tomcat: " + propertyValue;
+		String common = " Common properties > " + environment.getProperty("timezone") + " | " + environment.getProperty("city") + "\n";
+		return "Config as seen on client deployable on Tomcat: " + propertyValue + common;
 	}
 }
